9af5b7a77ba3d02d657df27188a1668d134eb4ca
Thalassocracy <42811940+Thalassocracy@users.noreply.github.com>
2019-01-21 15:51:02 -0500

    stuff

--- dev/python_scripts/gen_spawners/test_1.py
+++ dev/python_scripts/gen_spawners/test_1.py
@@ -4,13 +4,20 @@ import pprint
 with open('spawner.json') as spawnerfile:
     spawner = json.load(spawnerfile)
     json.dumps(spawner)
-    pprint.pprint(spawner)
-    # spawner.display_name = "!LOC: feed me"
-    # spawner.description = "brakelabasaasta"
-    # spawner.tools.projectiles_per_fire = 2
+    spawner['display_name'] = "!LOC: feed me"
+    spawner['description'] = "brakelabasaasta"
+    tools = spawner['tools'][0]
+    tools['projectiles_per_fire'] = 2
+    tools['spec_id'] = "/pa/units/spawner/firefly_spawner/firefly_spawner_weapon.json"
+    spawner['production']['metal'] = 25
+    spawner['production']['energy'] = 25
+    spawner['consumption']['energy'] = 10
+    spawner['unit_types'] = ["UNITTYPE_Structure", "swag"]
+    
     # spawner.tools.spec_id = "/pa/units/spawner/firefly_spawner/firefly_spawner_weapon.json"
     # spawner.production.metal = 25
     # spawner.production.energy = 25
     # spawner.consumption.energy = 10
     # spawner.unit_types = ["UNITTYPE_Structure", "swag"]
 
+v b
